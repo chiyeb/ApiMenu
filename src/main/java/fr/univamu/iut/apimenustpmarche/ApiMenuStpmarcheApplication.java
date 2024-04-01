13c3f63e0bd9d59a4d6e82ef60222e3fa3cce39d
chiheb bradai <chiheb.bradai@etu.univ-amu.fr>
2024-04-01 13:07:36 +0200

    i

---
+++ src/main/java/fr/univamu/iut/apimenustpmarche/ApiMenuStpmarcheApplication.java
@@ -0,0 +1,13 @@
+package fr.univamu.iut.apimenustpmarche;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMenuStpmarcheApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiMenuStpmarcheApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/univamu/iut/apimenustpmarche/ApiMenuStpmarcheApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.univamu.iut.apimenustpmarche;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMenuStpmarcheApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
